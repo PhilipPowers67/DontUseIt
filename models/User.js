09a99853d195d0512f2ece3ee93c52c98c06e732
Philip Powers <philip_powers607@hotmail.com>
2021-12-01 14:16:40 -0500

    models for User/Thoughts/Reactions

--- models/User.js
+++ models/User.js
@@ -1,3 +1,46 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-module.exports = Users;
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    // Use REGEX to validate Email...
+    match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false
+});
+
+//total count of friends
+UserSchema.virtuals('friendCount').get(function() {
+    return this.friends.length
+});
+
+//create User model using UserSchema
+const User = model('User', UserSchema);
+
+module.exports = User;
